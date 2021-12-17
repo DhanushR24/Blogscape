c61bd4982822284b80d3cbfe03aa58344074d514
Dhanush Raj <dhanushra66@gmail.com>
2021-12-17 06:35:14 +0530

    blog page design improvements :fire:

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -14,6 +14,16 @@ module.exports.get_blogs = (req, res) => {
     });
 }
 
+module.exports.get_all_blogs = (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.send(blogs);
+    });
+}
+
 module.exports.post_blog = (req, res) => {
     console.log(req.body);
     const {
--- routers/blogRoutes.js
+++ routers/blogRoutes.js
@@ -3,8 +3,10 @@ const router = Router();
 const blogController = require('../controllers/blogController')
 
 router.get('/blogs', blogController.get_blogs);
+router.get('/loadBlogs', blogController.get_all_blogs);
 
 router.get('/createBlog', blogController.get_create_blog);
 router.post('/createBlog', blogController.post_blog);
+
 
 module.exports = router;
