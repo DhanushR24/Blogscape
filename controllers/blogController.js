b68c0ae8e094ad68d0231be0b7e1daf963ed4de5
dineshkudva <dineshkudva12@gmail.com>
2021-12-24 14:36:56 +0530

    profile page updated with user blog content :fire:

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,75 +1,85 @@
-const Blog = require('../models/Blog');
+const Blog = require("../models/Blog");
 
 module.exports.get_blogs = (req, res) => {
-    Blog.find({}, (err, blogs) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        // res.send(blogs);
-        res.render('blogs', {
-            title: 'blogs'
-        });
+  Blog.find({}, (err, blogs) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    // res.send(blogs);
+    res.render("blogs", {
+      title: "blogs",
     });
-}
+  });
+};
 
 module.exports.get_all_blogs = (req, res) => {
-    Blog.find({})
-        .sort({
-            createdAt: -1
-        })
-        .then((blogs) => {
-            res.send(blogs);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}
+  Blog.find({})
+    .sort({
+      createdAt: -1,
+    })
+    .then((blogs) => {
+      res.send(blogs);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports.get_user_blogs = (req, res) => {
+  const username = req.params.user;
+
+  Blog.find({ username })
+    .sort({
+      createdAt: -1,
+    })
+    .then((blogs) => {
+      res.send(blogs);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 module.exports.post_blog = (req, res) => {
-    console.log(req.body);
-    const {
-        title,
-        description,
-        category,
-        username,
-        profile
-    } = req.body;
+  console.log(req.body);
+  const { title, description, category, username, profile } = req.body;
 
-    const blog = new Blog({
-        title,
-        description,
-        category,
-        username,
-        profile
-    });
+  const blog = new Blog({
+    title,
+    description,
+    category,
+    username,
+    profile,
+  });
 
-    blog.save((err) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log('Blog saved successfully');
-        res.redirect('blogs');
-    });
-}
+  blog.save((err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Blog saved successfully");
+    res.redirect("blogs");
+  });
+};
 
 module.exports.get_create_blog = (req, res) => {
-    res.render('createBlogs', {
-        title: 'Create Blog'
-    });
-}
+  res.render("createBlogs", {
+    title: "Create Blog",
+  });
+};
 
 module.exports.like_blog = (req, res) => {
-    const id = req.params.id;
-    let likes = parseInt(req.body.likes);
+  const id = req.params.id;
+  let likes = parseInt(req.body.likes);
 
-    Blog.findByIdAndUpdate(id, {
-        likes
-    })
+  Blog.findByIdAndUpdate(id, {
+    likes,
+  })
     .then((blog) => {
-        res.json(blog);
-    }).catch((err) => {
-        console.log(err);
+      res.json(blog);
+    })
+    .catch((err) => {
+      console.log(err);
     });
-}
+};
